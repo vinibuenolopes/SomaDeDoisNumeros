4599a3d44fd60979bb554717b8302a17636037e2
Vinicius Bueno Lopes <129338268+vinibuenolopes@users.noreply.github.com>
2024-04-08 21:11:53 -0300

    Add files via upload

---
+++ Parcelas/Program.cs
@@ -0,0 +1,12 @@
+﻿double valorCompra, numeroParcelas;
+Console.WriteLine("Digite o número de vezes pelo qual deseja parcelar");
+numeroParcelas = double.Parse(Console.ReadLine());
+if (numeroParcelas > 5){
+    Console.WriteLine("Parcelamos em até 5 vezes apenas");
+}else
+{
+    Console.WriteLine("Digite o valor da compra");
+    valorCompra = double.Parse(Console.ReadLine());
+    Console.WriteLine("Cada parcela custará {0:N} reais e serão {1} meses até o fim das parcelas",(valorCompra/numeroParcelas),numeroParcelas);
+}
+
